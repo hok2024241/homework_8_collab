cd2d13a5e87af20ddb849e9aba5fe98c70657c17
LaimengEung <2024440eung@aupp.edu.kh>
2025-04-18 16:24:15 +0700

    Submission for Exercise 4

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,17 @@
 def chunking_by(numbers, chunck):
-    ...
+    inside_list = []
+    chunked_list = []
+    
+    for i in numbers:
+        inside_list.append(i)
+        if len(inside_list) == chunck:
+            chunked_list.append(inside_list)
+            inside_list = []
+
+    if inside_list != []:
+        chunked_list.append(inside_list)
+
+    return chunked_list
+
+# Example Test
+print(chunking_by([5, 4, 7, 3, 4, 5, 4], 3))
