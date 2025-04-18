d9d1b674f96af70592bae665b67cb38b706a8e22
hok2024241 <2024241hok@aupp.edu.kh>
2025-04-18 19:28:31 +0700

    Update exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -1,6 +1,6 @@
 def index_power(numnbers, n):
-    def index_power(numnbers, n):
     if n < len(numnbers):
         return numnbers[n] ** n
     return -1
-
+print(index_power([1, 2, 3, 4], 2)) 
+print(index_power([1, 3, 10, 100], 3))   
